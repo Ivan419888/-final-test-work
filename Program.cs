c8c4f590571b9db01a8c0a312fc014b9abc30fc1
Ivan419888 <ivan419888@gmail.com>
2022-07-27 10:31:21 +0300

    Создал программу

--- Program.cs
+++ Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// array of strings
+
+string[] arrayInput = new string[4] { "hello", "2", "world", ":-)" };
+int size = 0;
+
+int indexInput = 0;
+int indexOutput = 0;
+
+while (indexInput < arrayInput.Length)
+{
+    if (arrayInput[indexInput].Length <= 3)
+    {
+        size++;
+    }
+    indexInput++;
+}
+
+string[] arrayOutput = new string[size];
+indexInput = 0;
+while (indexInput < arrayInput.Length)
+{
+    if (arrayInput[indexInput].Length <= 3)
+    {
+        arrayOutput[indexOutput] = arrayInput[indexInput];
+        indexOutput++;
+    }
+    indexInput++;
+}
+
+PrintStringArray(arrayInput);
+Console.Write(" -> ");
+PrintStringArray(arrayOutput);
+
+void PrintStringArray(string[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+
+        if (i != array.Length - 1)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.Write("]");
+}
